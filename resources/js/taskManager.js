66ad84c49834b97d3c6f4af00493bb55cf4c96cc
“IvanARodriguez” <ivancalvo91@gmail.com>
2022-04-07 20:45:42 -0500

    fixing the task manager class typo

--- resources/js/taskManager.js
+++ resources/js/taskManager.js
@@ -1,33 +1,30 @@
    export class TaskManager {
-    tasks;
-    //currentId = 0;
-    construtor(tasks=[]){
+    constructor(tasks=[], currentId = 0){
         this.tasks = tasks;
         this.currentId = 0;
-        
     }
 
-    // addTask(taskName, description, assignedTo, dueDate, taskStatus="Todo"){
-    //     this.currentId++;
+    addTask(taskName, description, assignedTo, dueDate, taskStatus="Todo"){
+        this.currentId++;
          
-    //     const newTask = {
-    //         id: this.currentId,
-    //         name: taskName,
-    //         description: description,
-    //         assignedTo: assignedTo,
-    //         dueDate: dueDate,
-    //         taskStatus: taskStatus 
+        const newTask = {
+            id: this.currentId,
+            name: taskName,
+            description: description,
+            assignedTo: assignedTo,
+            dueDate: dueDate,
+            taskStatus: taskStatus 
 
-    //     };
+        };
 
-    //     this.tasks.push(newTask);
-    // }
+        this.tasks.push(newTask);
+    }
 
 }
 
 
-//newTaskMan.addTask("feed your fish","dont over feed your fishes","Ivan",new Date().toDateString());
 const newTaskMan= new TaskManager();
-// console.log(newTaskMan());
 
-console.log(TaskManager.tasks)
+newTaskMan.addTask("feed your fish","dont over feed your fishes","Ivan",new Date().toDateString());
+
+console.log(newTaskMan.tasks)
